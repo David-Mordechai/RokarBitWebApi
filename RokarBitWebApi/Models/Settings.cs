1ff66b91fcd14d586649dae1dc8570b163ad4cb8
David Mordechai <David.mordechai79@gmail.com>
2023-03-14 19:10:37 +0200

    refactoring

--- RokarBitWebApi/Models/Settings.cs
+++ RokarBitWebApi/Models/Settings.cs
@@ -6,12 +6,3 @@ public class Settings
     public string IcdPath { get; set; }
     public SerialPort SerialPort { get; set; } = new();
 }
-
-public class SerialPort
-{
-    public string Name { get; set; } = "com0";
-    public long BindleRate { get; set; }
-    public string Parity { get; set; }
-    public string DataBits { get; set; }
-    public string StopBits { get; set;}
-}
